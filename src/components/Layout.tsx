c16dec50b492eb5dcd3c6afd16ccdefd3206a218
Tyler Joy <mrjoy206@gmail.com>
2024-02-29 10:12:04 -0800

    added Layout component

---
+++ src/components/Layout.tsx
@@ -0,0 +1,11 @@
+import React, { ReactNode } from "react";
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return <main>{children}</main>;
+};
+
+export default Layout;
